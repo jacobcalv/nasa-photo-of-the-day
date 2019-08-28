1b430e08deac14937d77b0daa4817da989248321
jacobcalv <jacobcalvino@outlook.com>
2019-08-28 17:39:52 -0500

    added props to app.js and added my nasa key api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import "./App.css";
 import PhotoOfDay from './components/component.js'
 
 function App (props) {
-
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=ztmQx32zY78Rm5DL5gbyKBjQYmY9uY9WOZ0YzEvS')
+  .then(response => {
+    console.log(response.data);
+  });
+return(
+    <div className='App'>
+        <PhotoOfDay title='hi' 
+                    url='https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg'
+                    details='deets'/>
+    </div>
   )
     
 };
