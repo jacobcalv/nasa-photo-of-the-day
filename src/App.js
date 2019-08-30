779da30cc0359dedd6d543c5b725658110de694c
jacobcalv <jacobcalvino@outlook.com>
2019-08-29 20:20:26 -0500

    add all the styling and made mobile design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+
 import "./App.css";
 
 import PhotoOfDay from './components/component.js'
 
 function App (props) {
   const [apiData, setApiData] = useState({});
-  useEffect(() => {
+
+  const [date, setDate] = useState("https://api.nasa.gov/planetary/apod?api_key=815oDd6HlGCMeBtD5DhRoJRoqkqQd1X6ftcObMPt");
+  const today = new Date();
+  const [dayOfWeek, setDayOfWeek] = useState(today.getDate())
+  console.log(today.getMonth()+1)
+  const backADay = () => {
+    setDate(date=> `https://api.nasa.gov/planetary/apod?api_key=815oDd6HlGCMeBtD5DhRoJRoqkqQd1X6ftcObMPt&date=${today.getFullYear()}-${today.getMonth() + 1}-${dayOfWeek -1}`)
+  }
+  useEffect((props) => {
     console.log('render check')
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=815oDd6HlGCMeBtD5DhRoJRoqkqQd1X6ftcObMPt&date=2019-07-15')
+    axios.get(date)
       .then(response => {
         setApiData(response.data);
       });
-  }, [])
+  }, [date])
 
 return(
     <div className='App'>
-        <PhotoOfDay title={apiData.title}
+        <PhotoOfDay back={'Missed Yesterday? Click Here!'}
+                    dateFunc={backADay}
+                    title={apiData.title}
                     date={apiData.date} 
                     url= {apiData.url}
                     details={apiData.explanation}/>
--- src/components/component.js
+++ src/components/component.js
@@ -1,16 +1,82 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SpaceHolder = styled.div`
+    text-align: center;
+    background-color: black;
+    margin-top: none;
+    margin-bottom: none;
+`
+const SpaceHeader = styled.header`
+    width: 100%;
+`
+const Back = styled.button`
+    border= 3px solid white;
+    background-color: black;
+    color: white;
+`
+
+const SpaceTitle = styled.h1`
+    font-family: 'Space Mono', monospace;
+    font-size: 2rem;
+    width: 90%;
+    margin-left: 5%;
+    padding-top: 2rem;
+    color: white;
+`
+
+const SpaceDate = styled.h2`
+    color: white;
+    font-family:'Space Mono', monospace; 
+`
+const SpaceContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 5rem;
+    @media(max-width: 600px){
+        display: flex;
+        flex-direction: column;
+        padding-bottom: 2rem;
+    }
+`
+const SpaceImage = styled.img`
+    width: 45%;
+    @media(max-width: 600px){
+        width: 80%;
+    }
+`
+
+const SpaceDetails = styled.p`
+    display: flex;
+    width: 40%;
+    text-align: center;
+    margin-left: 3rem;
+    color: white;
+    font-family: 'Space Mono', monospace;
+    font-size: 1.1rem;
+    @media(max-width: 600px){
+        width: 80%;
+        text-align: center;
+        margin-left: .3rem;
+    }
+`
+
 
 function PhotoOfDay(props) {
     return(
-        <div>
-            <h2 className='title'>{props.title}</h2>
-            <h3 className='date'>{props.date}</h3>
-            <div className='container'>
-                <img className='image' src={props.url}/>
-                <p className='details'>{props.details}</p> 
-            </div>
-            
-        </div>
+        <SpaceHolder>
+            <SpaceHeader>
+            <Back onClick={props.dateFunc}>{props.back}</Back>
+            </SpaceHeader>
+            <SpaceTitle>{props.title}</SpaceTitle>
+            <SpaceDate>{props.date}</SpaceDate>
+            <SpaceContainer>
+                <SpaceImage src={props.url}/>
+                <SpaceDetails>{props.details}</SpaceDetails> 
+            </SpaceContainer>
+        </SpaceHolder>    
     )
 }
 
